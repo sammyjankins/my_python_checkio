1a5d9b245f7e57b13aa204f5c601169858fa6597
Олег Семенов <neroragno@mail.ru>
2018-08-20 11:33:45 +0700

    Init army battles

---
+++ Electronic Station/army_battles.py
@@ -0,0 +1,34 @@
+
+
+# chuck = Warrior()
+# bruce = Warrior()
+# carl = Knight()
+# dave = Warrior()
+# mark = Warrior()
+#
+# fight(chuck, bruce) == True
+# fight(dave, carl) == False
+# chuck.is_alive == True
+# bruce.is_alive == False
+# carl.is_alive == True
+# dave.is_alive == False
+# fight(carl, mark) == False
+# carl.is_alive == False
+#
+# my_army = Army()
+# my_army.add_units(Knight, 3)
+#
+# enemy_army = Army()
+# enemy_army.add_units(Warrior, 3)
+#
+# army_3 = Army()
+# army_3.add_units(Warrior, 20)
+# army_3.add_units(Knight, 5)
+#
+# army_4 = Army()
+# army_4.add_units(Warrior, 30)
+#
+# battle = Battle()
+#
+# battle.fight(my_army, enemy_army) == True
+# battle.fight(army_3, army_4) == False
